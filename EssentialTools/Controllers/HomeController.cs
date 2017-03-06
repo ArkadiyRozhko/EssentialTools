adabc5b896b0416cae19199c52d637d7b186fbe3
Аркадій <arkasha9@gmail.com>
2017-03-06 18:08:33 +0200

    Add project files.

---
+++ EssentialTools/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EssentialTools.Models;
+using Ninject;
+
+namespace EssentialTools.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IValueCalculator calc;
+
+        private Product[] products =
+        {
+            new Product {Name = "Каяк", Category = "Водные виды спорта", Price = 350M},
+            new Product {Name = "Спасательный жилет", Category = "Водные виды спорта", Price = 200M},
+            new Product {Name = "Мяч", Category = "Футбол", Price = 120M},
+            new Product {Name = "Угловой флажок", Category = "Футбол", Price = 75M}
+        };
+
+
+
+        public HomeController(IValueCalculator calcParam)
+        {
+            calc = calcParam;
+        }
+        public ActionResult Index()
+        {
+            
+            ShopingCart cart=new ShopingCart(calc)
+            {
+                Products = products
+            };
+           
+            decimal totalValue = cart.CalculateProductsTotal();
+
+            return View(totalValue);
+        }
+    }
+}
---
+++ EssentialTools/Models/ShopingCart.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EssentialTools.Models
+{
+    public class ShopingCart
+    {
+        private IValueCalculator calc;
+
+        public ShopingCart(IValueCalculator calcParam)
+        {
+            calc = calcParam;
+        }
+        public IEnumerable<Product> Products { get; set; }
+
+        public decimal CalculateProductsTotal()
+        {
+            return calc.ValueProducts(Products);
+        }
+    }
+}
